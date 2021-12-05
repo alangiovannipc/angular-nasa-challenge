6de60ded4713836e97d8125c7ffc21413b5fedf5
alangiovannipc <alangiovannipc@gmail.com>
2021-12-05 14:13:16 -0500

    Update Search Component

--- src/components/Nasa/Search/search.component.ts
+++ src/components/Nasa/Search/search.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, Input, OnInit } from '@angular/core';
+import { Rover, Camera, SearchRequest } from '../shared/nasa.types';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css'],
 })
-export class SearchComponent {}
+export class SearchComponent implements OnInit {
+  @Output() onSearch = new EventEmitter<SearchRequest>();
+  @Input() roverSelected: Rover;
+  @Input() cameraList: Camera[];
+
+  ngOnInit(): void {
+    console.log('SearchComponent ');
+  }
+}
