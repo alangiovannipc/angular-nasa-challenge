072f86d73be1259981c27e3d9bc0fb9e98ccea4e
alangiovannipc <alangiovannipc@gmail.com>
2021-12-05 13:23:30 -0500

    Update Filter component

--- src/components/Nasa/Filter/filter.component.ts
+++ src/components/Nasa/Filter/filter.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
   styleUrls: ['./filter.component.css'],
 })
-export class FilterComponent {}
+export class FilterComponent {
+  @Output() onFilter = new EventEmitter<string>();
+
+  handleFilter(cameraName: string) {
+    console.log('onFilter cameraName ', cameraName);
+    this.onFilter.emit(cameraName);
+  }
+}
