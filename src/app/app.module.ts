8b0dcb85ce75d03b2773eb0883bc9799005e41cc
alangiovannipc <alangiovannipc@gmail.com>
2021-12-05 12:49:48 -0500

    Update List Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PageModule } from '../pages';
 import { AppRoutingModule } from './app-routing.module';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  imports: [BrowserModule, PageModule, AppRoutingModule],
+  imports: [BrowserModule, PageModule, AppRoutingModule, CommonModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
