3e3c7c763d0f5554101e1650aec7d6b20b69974d
alangiovannipc <alangiovannipc@gmail.com>
2021-12-05 22:40:40 -0500

    Add libraries

---
+++ src/libraries/bootstrap/bootstrap.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import {
+  NgbPaginationModule,
+  NgbAlertModule,
+} from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  imports: [NgbPaginationModule, NgbAlertModule],
+})
+export class BootstrapModule {}
